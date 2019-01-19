23433df7258ba2a7818805b6dfd6f4a18c75cb00
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2019-01-19 13:36:38 -0500

    Update main.dart

--- pestguestarrest_app/lib/main.dart
+++ pestguestarrest_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 void main() => runApp(
     MaterialApp(
@@ -15,17 +16,71 @@ class myState extends StatefulWidget {
 
 class _myStateState extends State<myState> {
 
-  bool _value1 = false;
-  bool _value2 = false;
+  String status = "Unlocked";
+  bool value1 = false;
 
-  void _onChanged1(bool value) => setState(() => _value1 = value);
-  void _onChanged2(bool value) => setState(() => _value2 = value);
+  Widget get head
+  {
+    return Container(
+      decoration: BoxDecoration(border: new Border.all(color: Colors.black), color: Colors.grey),
+      width: 20.0,
+      height: 20.0,
+    );
+  }
+
+  //Widget that contains text and switch
+  Widget get changeStatus
+  {
+    return Column(
+      children: <Widget>[
+        head,
+        new Container(
+            child: Text("Lock/unlock your car", style: TextStyle(fontSize: 20.0, color: Colors.green)),
+          decoration: new BoxDecoration(
+            border: new Border.all(color: Colors.red),
+          ),
+        ),
+        switchStatus,
+        Text(status),
+      ],
+    );
+  }
+
+  //Switch widget
+  Widget get switchStatus
+  {
+    return new Switch(value: value1, onChanged: (value)=> change());
+  }
+  change(){
+    setState(() {
+      if(value1 == false)
+        {
+          status = "Locked";
+        }
+      else
+        {
+          status = "Unlocked";
+        }
+      value1 =! value1;
+    });
+  }
+
+  //Settings
+  Widget get settings
+  {
+    return IconButton(icon: Icon(Icons.settings), onPressed: bb());
+  }
+
+  bb()
+  {
+    print("hello");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Pest Guest Arrest")),
+        title: Center(child: Text("Arrest the Pest Guest!")),
         backgroundColor: Colors.red,
         elevation: 0.0,
       ),
@@ -58,15 +113,11 @@ class _myStateState extends State<myState> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 160.0, bottom: 20.0),
-              child: Text("Lock/Unlock object", style: TextStyle(color: Colors.red, fontSize: 20.0),),
-            ),
-            new Switch(value: _value1, onChanged: _onChanged1),
+            changeStatus,
+            settings,
           ],
         ),
       ),
     );
   }
-
 }
